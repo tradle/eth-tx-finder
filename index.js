0956af307fd33352a10a6574edad9ef040d6f239
Mark Vayngrib <mark@tradle.io>
2017-04-16 21:46:39 -0400

    support brute force findAllTxsTo

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
 const EthQuery = require('eth-store/query')
 const async = require('async')
 const findIncrements = require('./disect.js')
+const MAX_CONCURRENT = 5
 
 module.exports = {
   findAllTxs,
   findAllTxsInRange,
+  findAllTxsTo,
+  findAllTxsInRangeTo,
 }
 
+function rangeToArray (min, max) {
+  var arr = []
+  for (var i = min; i <= max; i++) {
+    arr.push(i)
+  }
+
+  return arr
+}
+
+function flatten (arr) {
+  return arr.reduce(function (all, some) {
+    return all.concat(some)
+  }, [])
+}
+
+function findAllTxsTo(provider, targetAddress, onTx, onComplete) {
+  var query = new EthQuery(provider)
+  query.getLatestBlockNumber(function(err, blockNumberHex){
+    if (err) return onComplete(err)
+    var blockNumber = parseInt(blockNumberHex, 16)
+    findAllTxsInRangeTo(provider, targetAddress, 0, blockNumber, onTx, onComplete)
+  })
+}
+
+function findAllTxsInRangeTo(provider, targetAddress, minBlockNumber, maxBlockNumber, onTx, onComplete) {
+  var query = new EthQuery(provider)
+  query.getLatestBlockNumber(function(err, blockNumberHex){
+    if (err) return onComplete(err)
+    var blockNumber = parseInt(blockNumberHex, 16)
+    async.mapLimit(rangeToArray(minBlockNumber, maxBlockNumber), MAX_CONCURRENT, function (blockNumber, cb) {
+      query.getBlockByNumber(blockNumber, function(err, block){
+        if (err) return onComplete(err)
+        var matchingTxs = block.transactions.filter(tx => tx.to === targetAddress)
+        matchingTxs.forEach(onTx)
+        cb(null, matchingTxs)
+      })
+    }, function (err, results) {
+      if (err) return onComplete(err)
+
+      onComplete(null, flatten(results))
+    })
+  })
+}
 
 function findAllTxs(provider, targetAddress, onTx, onComplete){
   var query = new EthQuery(provider)
